e89c0e4a945eadfe12de5d89dfbaab03abeae162
El Jedaaoui Hachem <heljedaaoui@exadel.by>
2018-11-06 13:45:15 +0200

    list view created

---
+++ js/views/list.js
@@ -0,0 +1,14 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+], function ($, _, Backbone) {
+    var ListView = Backbone.View.extend({
+        el: $('.main__display'),
+        initialize: function() {
+        },
+        render: function() {
+        }
+    });
+    return ListView;
+});
