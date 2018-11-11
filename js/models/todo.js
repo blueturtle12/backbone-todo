a6161b63f4faae6647f21754ebb06ba4f78c8a84
hachem jadaoui <blueturtle1293@gmail.com>
2018-11-11 19:57:37 +0200

    added on button click timer start

--- js/models/todo.js
+++ js/models/todo.js
@@ -7,7 +7,9 @@ define([
 			title: '',
 			discription: '',
             completed: false,
-            time: ''
+			time: '',
+			minutes: '',
+			seconds: ''
 		},
 		// interval: setInterval(function() {
 		// 	i++
--- js/views/entry.js
+++ js/views/entry.js
@@ -9,13 +9,21 @@ define([
         initialize: function() {
             this.template = _.template($(".todoTemplate").html());
             this.model.attributes.time = this.model.attributes.time * 60;
+            this.model.attributes.minutes = Math.floor(this.model.attributes.time/60);
+            this.model.attributes.seconds = this.model.attributes.time%60;
+        },
+
+        events: {
+            "click .todo__btn": "onStart"
+        },
+
+        onStart: function() {
             var time = this.model.attributes.time + 1;
             var self = this;
             var timerLoop = setInterval(function x() {
                     time--
-                    var minutes = Math.floor(time/60);
-                    var seconds = time%60;
-                    self.model.attributes.time = minutes + ':' + seconds;
+                    self.model.attributes.minutes = Math.floor(time/60);
+                    self.model.attributes.seconds = time%60;
                     self.render();
                     if(time === 0){
                         clearInterval(timerLoop);
@@ -23,7 +31,6 @@ define([
                 return x;
             }(),1000);
         },
-
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
