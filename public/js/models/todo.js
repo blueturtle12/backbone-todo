3beed32a380bf02b1b586f6ac541c8927c5ed6ad
El Jedaaoui Hachem <heljedaaoui@exadel.by>
2018-11-12 12:26:31 +0200

    added new lines to the end of each document

--- public/js/models/todo.js
+++ public/js/models/todo.js
@@ -1,29 +1,13 @@
-define([
-	'underscore',
-	'backbone'
-], function (_, Backbone) {
+define(["underscore", "backbone"], function(_, Backbone) {
 	var Todo = Backbone.Model.extend({
 		defaults: {
-			title: '',
-			description: '',
-            completed: false,
-			time: '',
-			minutes: '',
-			seconds: ''
-		},
-		// interval: setInterval(function() {
-		// 	i++
-		// 	console.log(i);
-		// 	if(i === parseInt(this.time)){
-		// 		clearInterval();
-		// 	}
-		// 	this.update(i);
-		// },1000),
-		// update: function(x) {
-		// 	this.set({
-		// 		time: x
-		// 	})
-		// }
+			title: "",
+			description: "",
+			completed: false,
+			time: "",
+			minutes: "",
+			seconds: ""
+		}
 	});
 
 	return Todo;
